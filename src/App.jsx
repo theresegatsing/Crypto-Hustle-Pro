aa8368cc7296f1acc52161f3f5fd78222db6b2e8
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-14 16:27:57 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import CoinInfo from "./Components/CoinInfo"
 
+
 const API_KEY = import.meta.env.VITE_APP_API_KEY
 
 function App() {
   const [list, setList] = useState(null)
+  const [filteredResults, setFilteredResults] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+
   useEffect(() => {
     const fetchAllCoinData = async () => {
       const response = await fetch(
@@ -20,6 +24,21 @@ function App() {
     fetchAllCoinData().catch(console.error)
 
   }, [])
+
+  const searchItems = searchValue => {
+      setSearchInput(searchValue)
+      if (searchValue !== "") {
+        const filteredData = Object.keys(list.Data).filter((item) => 
+          Object.values(item)
+            .join("")
+            .toLowerCase()
+            .includes(searchValue.toLowerCase())
+        )
+        setFilteredResults(filteredData)
+      } else {
+        setFilteredResults(Object.keys(list.Data))
+      }
+  }
 
   return (
     <div className="whole-page">
@@ -48,6 +67,44 @@ function App() {
           
         </ul>
     </div>
+
+    {searchInput.length > 0
+      ? filteredResults
+        .map((coin) => {
+          const coinData = list.Data[coin]
+          if (
+            coinData.IsTrading &&
+            coinData.Algorithm !== "N/A" &&
+            coinData.ProofType !== "N/A"
+          ) {
+            return (
+              <CoinInfo
+                key={coin}
+                image={coinData.ImageUrl}
+                name={coinData.FullName}
+                symbol={coinData.Symbol}
+              />
+            )
+          }
+          return null
+        })
+    : list &&
+      Object.entries(list.Data)
+        .filter(
+          ([_, coinData]) =>
+            coinData.isTrading &&
+            coinData.Algorithm !== "N/A" &&
+            coinData.ProofType !== "N/A"
+        )
+        .slice(0, 20)
+        .map(([coin, coinData]) => (
+          <CoinInfo
+            key={coin}
+            image={coinData.ImageUrl}
+            name={coinData.FullName}
+            symbol={coinData.Symbol}
+          />
+        ))}
     
   )
 }
