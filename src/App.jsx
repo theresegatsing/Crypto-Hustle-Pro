9d14c1b55eff2cef0c863745db40f20d5fe0fdaa
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-14 16:30:56 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState,  useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CoinInfo from "./Components/CoinInfo"
-
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY
 
@@ -14,30 +13,27 @@ function App() {
   useEffect(() => {
     const fetchAllCoinData = async () => {
       const response = await fetch(
-    "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" 
-    + API_KEY
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
       )
-
       const json = await response.json()
       setList(json)
     }
     fetchAllCoinData().catch(console.error)
-
   }, [])
 
   const searchItems = searchValue => {
-      setSearchInput(searchValue)
-      if (searchValue !== "") {
-        const filteredData = Object.keys(list.Data).filter((item) => 
-          Object.values(item)
-            .join("")
-            .toLowerCase()
-            .includes(searchValue.toLowerCase())
-        )
-        setFilteredResults(filteredData)
-      } else {
-        setFilteredResults(Object.keys(list.Data))
-      }
+    setSearchInput(searchValue)
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) =>
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData)
+    } else {
+      setFilteredResults(Object.keys(list.Data))
+    }
   }
 
   return (
@@ -48,64 +44,63 @@ function App() {
         placeholder="Search..."
         onChange={(inputString) => searchItems(inputString.target.value)}
       />
-        <ul>
+      <ul>
+        {list &&
+          Object.entries(list.Data)
+            .filter(([_, coinData]) =>
+              coinData.IsTrading &&
+              coinData.Algorithm !== "N/A" &&
+              coinData.ProofType !== "N/A"
+            )
+            .map(([coin, coinData]) => (
+              <CoinInfo
+                image={list.Data[coin].ImageUrl}
+                name={list.Data[coin].FullName}
+                symbol={list.Data[coin].Symbol}
+              />
+            ))}
+      </ul>
 
-          {list &&
-            Object.entries(list.Data)
-              .filter(([_, coinData]) =>
+      {/* ✅ Moved this JSX block inside the <div> to prevent top-level sibling error */}
+      <ul>
+        {searchInput.length > 0
+          ? filteredResults.map((coin) => {
+              const coinData = list.Data[coin]
+              if (
                 coinData.IsTrading &&
                 coinData.Algorithm !== "N/A" &&
                 coinData.ProofType !== "N/A"
+              ) {
+                return (
+                  <CoinInfo
+                    key={coin}
+                    image={coinData.ImageUrl}
+                    name={coinData.FullName}
+                    symbol={coinData.Symbol}
+                  />
+                )
+              }
+              return null
+            })
+          : list &&
+            Object.entries(list.Data)
+              .filter(
+                ([_, coinData]) =>
+                  coinData.IsTrading &&
+                  coinData.Algorithm !== "N/A" &&
+                  coinData.ProofType !== "N/A"
               )
+              .slice(0, 20)
               .map(([coin, coinData]) => (
                 <CoinInfo
-                  image={list.Data[coin].ImageUrl}
-                  name={list.Data[coin].FullName}
-                  symbol={list.Data[coin].Symbol}
+                  key={coin}
+                  image={coinData.ImageUrl}
+                  name={coinData.FullName}
+                  symbol={coinData.Symbol}
                 />
               ))}
-          
-        </ul>
+      </ul>
     </div>
-
-    {searchInput.length > 0
-      ? filteredResults
-        .map((coin) => {
-          const coinData = list.Data[coin]
-          if (
-            coinData.IsTrading &&
-            coinData.Algorithm !== "N/A" &&
-            coinData.ProofType !== "N/A"
-          ) {
-            return (
-              <CoinInfo
-                key={coin}
-                image={coinData.ImageUrl}
-                name={coinData.FullName}
-                symbol={coinData.Symbol}
-              />
-            )
-          }
-          return null
-        })
-    : list &&
-      Object.entries(list.Data)
-        .filter(
-          ([_, coinData]) =>
-            coinData.isTrading &&
-            coinData.Algorithm !== "N/A" &&
-            coinData.ProofType !== "N/A"
-        )
-        .slice(0, 20)
-        .map(([coin, coinData]) => (
-          <CoinInfo
-            key={coin}
-            image={coinData.ImageUrl}
-            name={coinData.FullName}
-            symbol={coinData.Symbol}
-          />
-        ))}
-    
   )
 }
 
