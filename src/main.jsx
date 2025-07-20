9e1d87678fac76261a3e73cd8afc731a497da6fe
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-20 00:45:40 -0400

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from "./routes/Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+<BrowserRouter>
+  <Routes>
+    <Route path ="/" element={<Layout />}>
+      <Route index = {true} path = "/" element = {<App />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
